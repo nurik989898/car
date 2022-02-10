5d9bf97835fd6f85863a00b5c5033d8c3f820d83
Nurik <nturdukulov911@gmail.com>
2022-02-10 16:49:28 +0600

    add glide e.t.c

--- app/src/main/java/com/example/a3rd_month/MainActivity.java
+++ app/src/main/java/com/example/a3rd_month/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.a3rd_month;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        imageView1 = findViewById(R.id.image_car);
+
+
+        String url ="https://i.pinimg.com/474x/23/ab/a6/23aba60b66ef08174bb7455c4a8a2d2f.jpg";
+        Glide.with(MainActivity.this)
+                .load(url)
+                .into(imageView1);
+}
 }
